9df602117efba83c872978c0f1a81e98cacedb80
Rahaem <r.emami98@gmail.com>
2025-04-15 20:36:04 -0700

    carousle spinner added

--- src/components/MovieCarousel3D.tsx
+++ src/components/MovieCarousel3D.tsx
@@ -3,39 +3,51 @@ import { Movie } from "../types/types";
 import { Image, Box } from "@chakra-ui/react";
 import "keen-slider/keen-slider.min.css";
 import "./MovieCarousel3D.css";
+import { useState, useEffect } from "react";
+import { Spinner } from "@chakra-ui/react";
 
 type Props = {
   movies: Movie[];
 };
 
 const carousel: KeenSliderPlugin = (slider) => {
-  const z = 300;
-  function rotate() {
+  const z = 300; // distance from the center of the carousel to the slides
+  const rotate = () => {
+    //slider.track.details.progress is a value from 0 to 1 (0% to 100% of scroll progress)
+    //It rotates the entire container while pushing it back z units to create a 3D effect.
     const deg = 360 * slider.track.details.progress;
     slider.container.style.transform = `translateZ(-${z}px) rotateY(${-deg}deg)`;
   }
   slider.on("created", () => {
-    const deg = 360 / slider.slides.length;
+    const deg = 360 / slider.slides.length; // spaces slides evenly around a circle.
     slider.slides.forEach((element, idx) => {
       element.style.transform = `rotateY(${deg * idx}deg) translateZ(${z}px)`;
     });
-    rotate();
+    rotate(); //calls to position the initial state.
   });
-  slider.on("detailsChanged", rotate);
+  slider.on("detailsChanged", rotate); //This updates the rotation whenever the slide details change like dragging
 };
 
 export const MovieCarousel3D = (props: Props) => {
+  const [loading, setLoading] = useState<Boolean>(true);
+  useEffect(() => {
+    if (props.movies.length > 0) {
+      setLoading(false);
+    }
+  });
   const [sliderRef] = useKeenSlider(
     {
-      loop: true,
-      selector: ".carousel__cell",
-      renderMode: "custom",
+      loop: true, //makes the carousel infinite
+      selector: ".carousel__cell", //which elements to treat as slides
+      renderMode: "custom", //allows some free scrolling but snaps to slides
       mode: "free-snap",
     },
     [carousel]
   );
 
-  return (
+  return loading ? (
+    <Spinner size="md" />
+  ) : (
     <Box className="wrapper">
       <Box className="scene">
         <Box className="carousel keen-slider" ref={sliderRef}>
